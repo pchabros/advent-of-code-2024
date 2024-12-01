de92ab03214e41e1a56a79ef95165a6232f97b64
pchabros <pawel.chabros@yahoo.pl>
2024-12-01 21:28:20 +0100

    Day 1: solutions moved to separate file

--- src/Day1.hs
+++ src/Day1.hs
@@ -5,6 +5,11 @@ import Data.List (sort, transpose)
 inputPath :: String
 inputPath = "./inputs/day-1.txt"
 
+-- Puzzle 1
+
+parseIdsPair :: String -> [Integer]
+parseIdsPair line = map read $ words line
+
 sortIdLists :: [[Integer]] -> [[Integer]]
 sortIdLists pairs = transpose $ map sort $ transpose pairs
 
@@ -13,5 +18,23 @@ calculatePairDistance [] = 0
 calculatePairDistance [_] = 0
 calculatePairDistance (id1 : id2 : _) = abs (id1 - id2)
 
-solution1 :: String
-solution1 = "foo"
+solve1 :: String -> Integer
+solve1 input = sum $ map calculatePairDistance $ sortIdLists pairs
+  where
+    pairs = map parseIdsPair $ lines input
+
+-- Puzzle 2
+
+parseColumns :: String -> ([Integer], [Integer])
+parseColumns input = (columns !! 0, columns !! 1)
+  where
+    columns = transpose $ map (map read . words) $ lines input
+
+count :: Integer -> [Integer] -> Integer
+count x = toInteger . length . filter (== x)
+
+calculateScore :: ([Integer], [Integer]) -> Integer
+calculateScore (col1, col2) = foldl (\acc number -> acc + number * count number col2) 0 col1
+
+solve2 :: String -> Integer
+solve2 input = calculateScore $ parseColumns input
--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,8 @@
 module Main where
 
-import Data.List (transpose)
-import Day1 (inputPath)
-
-parseColumns :: String -> ([Integer], [Integer])
-parseColumns input = (columns !! 0, columns !! 1)
-  where
-    columns = transpose $ map (map read . words) $ lines input
-
-count :: Integer -> [Integer] -> Integer
-count x = toInteger . length . filter (== x)
-
-calculateScore :: ([Integer], [Integer]) -> Integer
-calculateScore (col1, col2) = foldl (\acc number -> acc + number * count number col2) 0 col1
+import Day1 (inputPath, solve2)
 
 main :: IO ()
 main = do
   input <- readFile inputPath
-  print $ calculateScore $ parseColumns input
+  print $ solve2 input
