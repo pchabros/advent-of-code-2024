af38987b21a61d49f09d213e4025cab58d015a89
pchabros <pawel.chabros@yahoo.pl>
2024-12-02 15:44:40 +0100

    Day 2: first puzzle solved

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,27 @@
 module Main where
 
-import Day1 (inputPath, solve2)
+inputPath :: String
+inputPath = "./inputs/day-2.txt"
+
+parseReport :: String -> [Integer]
+parseReport = map read . words
+
+rolling :: (Integer -> Integer -> Integer) -> [Integer] -> [Integer]
+rolling f xs = zipWith f xs (tail xs)
+
+rollingDiff :: [Integer] -> [Integer]
+rollingDiff = rolling (-)
+
+between :: Integer -> Integer -> Integer -> Bool
+between lower upper x = x >= lower && x <= upper -- TODO: try to write without `x`
+
+stepsSafe :: [Integer] -> Bool
+stepsSafe steps = all (between 1 3) steps || all (between (-3) (-1)) steps
+
+isReportValid :: [Integer] -> Bool
+isReportValid report = stepsSafe $ rollingDiff report
 
 main :: IO ()
 main = do
   input <- readFile inputPath
-  print $ solve2 input
+  print $ length $ filter isReportValid $ map parseReport (lines input)
