490d61799155ae2ab4886778db3cd83f89e30b52
pchabros <pawel.chabros@yahoo.pl>
2024-12-02 23:14:09 +0100

    Day 2: second puzzle solved

--- src/Main.hs
+++ src/Main.hs
@@ -3,8 +3,20 @@ module Main where
 inputPath :: String
 inputPath = "./inputs/day-2.txt"
 
+between :: Integer -> Integer -> Integer -> Bool
+between lower upper x = x >= lower && x <= upper -- TODO: try to write without `x`
+
 parseReport :: String -> [Integer]
 parseReport = map read . words
+
+deleteNth :: Integer -> [Integer] -> [Integer]
+deleteNth _ [] = []
+deleteNth n (x : xs)
+  | n == 0 = xs
+  | otherwise = x : deleteNth (n - 1) xs
+
+getSubsets :: [Integer] -> [[Integer]]
+getSubsets set = map (`deleteNth` set) [0 .. (toInteger $ length set - 1)]
 
 rolling :: (Integer -> Integer -> Integer) -> [Integer] -> [Integer]
 rolling f xs = zipWith f xs (tail xs)
@@ -12,16 +24,18 @@ rolling f xs = zipWith f xs (tail xs)
 rollingDiff :: [Integer] -> [Integer]
 rollingDiff = rolling (-)
 
-between :: Integer -> Integer -> Integer -> Bool
-between lower upper x = x >= lower && x <= upper -- TODO: try to write without `x`
-
-stepsSafe :: [Integer] -> Bool
-stepsSafe steps = all (between 1 3) steps || all (between (-3) (-1)) steps
-
 isReportValid :: [Integer] -> Bool
-isReportValid report = stepsSafe $ rollingDiff report
+isReportValid report = isValid report || isValid (reverse report)
+  where
+    isValid r = all (between 1 3) $ rollingDiff r
+
+isDampenerValid :: [Integer] -> Bool
+isDampenerValid report = any isReportValid $ getSubsets report
+
+solve2 :: String -> Int
+solve2 input = length $ filter isDampenerValid $ map parseReport (lines input)
 
 main :: IO ()
 main = do
   input <- readFile inputPath
-  print $ length $ filter isReportValid $ map parseReport (lines input)
+  print $ solve2 input
