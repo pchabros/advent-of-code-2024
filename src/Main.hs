2acb15e0b7ab707aa910707e0332a3ac8e6569b0
pchabros <pawel.chabros@yahoo.pl>
2024-12-04 23:34:42 +0100

    Day 4: second puzzle solved

--- src/Main.hs
+++ src/Main.hs
@@ -34,7 +34,29 @@ solve1 input = sumLines matrix + sumLines (transpose matrix) + sumDiagonals matr
     sumDiagonals m = sumLines $ diagonals m
     matrix = map encode $ lines input
 
+-- ------------------------------------------------------------------------------------------------------------------
+
+countXMas' :: Int -> Int -> [Int] -> Int
+countXMas' idx width xs
+  | length xs < (2 * width + 2) = 0
+  | foundXMas xs = 1 + countXMas' (idx + 1) width (tail xs)
+  | otherwise = countXMas' (idx + 1) width (tail xs)
+  where
+    foundXMas xs' = notInNextLine && (head xs' == 2) && (xs' !! 2 == 2) && (xs' !! (width + 1) == 3) && (xs' !! (2 * width) == 4) && (xs' !! (2 * width + 2) == 4)
+    notInNextLine = idx `mod` width <= width - 2
+
+countXMas :: [[Int]] -> Int
+countXMas matrix = countXMas' 1 width xs
+  where
+    width = length $ head matrix
+    xs = concat matrix
+
+solve2 :: String -> Int
+solve2 input = countXMas matrix + countXMas (reverse matrix) + countXMas (transpose matrix) + countXMas (transpose $ reverse matrix)
+  where
+    matrix = map encode $ lines input
+
 main :: IO ()
 main = do
   input <- readFile inputPath
-  print $ solve1 input
+  print $ solve2 input
