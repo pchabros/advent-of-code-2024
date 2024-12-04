051bbbc43cfcbb2d4d3789b1be721e96222f70db
pchabros <pawel.chabros@yahoo.pl>
2024-12-04 21:19:20 +0100

    Day 4: first puzzle solved

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,38 @@
 module Main where
 
-inputPath :: String
-inputPath = "./inputs/day-3.txt"
+import Data.List (isPrefixOf, transpose)
 
-solve1 :: String -> Integer
-solve1 input = 1
+inputPath :: String
+inputPath = "./inputs/day-4.txt"
+
+enumerate :: [a] -> [(Int, a)]
+enumerate = zip [0 ..]
+
+encode :: String -> [Int]
+encode = map encode'
+  where
+    encode' 'X' = 1
+    encode' 'M' = 2
+    encode' 'A' = 3
+    encode' 'S' = 4
+    encode' _ = 0
+
+xmasCode :: [Int]
+xmasCode = [1 .. 4]
+
+diagonals :: [[Int]] -> [[Int]]
+diagonals matrix = transpose $ map (\(i, row) -> replicate i 0 ++ row) (enumerate matrix)
+
+count :: [Int] -> Int
+count [] = 0
+count xs = (if xmasCode `isPrefixOf` xs || reverse xmasCode `isPrefixOf` xs then 1 else 0) + count (tail xs)
+
+solve1 :: String -> Int
+solve1 input = sumLines matrix + sumLines (transpose matrix) + sumDiagonals matrix + sumDiagonals (map reverse matrix)
+  where
+    sumLines m = sum $ map count m
+    sumDiagonals m = sumLines $ diagonals m
+    matrix = map encode $ lines input
 
 main :: IO ()
 main = do
