5b3d57e5768a74c0d3a6e166d49a3da6b4fb4a54
pchabros <pawel.chabros@yahoo.pl>
2024-12-04 18:42:59 +0100

    Day 3: solutions moved to separate file

--- src/Main.hs
+++ src/Main.hs
@@ -1,37 +1,12 @@
 module Main where
 
-import Text.Regex.TDFA
-
 inputPath :: String
 inputPath = "./inputs/day-3.txt"
 
-findMul :: String -> [String]
-findMul str = getAllTextMatches (str =~ "mul\\([0-9]{1,3},[0-9]{1,3}\\)")
-
-findMultipliers :: String -> [String]
-findMultipliers str = getAllTextMatches (str =~ "[0-9]{1,3}")
-
-multiply :: [String] -> Integer
-multiply multipliers = product parsed
-  where
-    parsed = map read multipliers
-
-findDisabled :: String -> (String, String, String)
-findDisabled input = input =~ "don't\\(\\)(.|\n)*?do\\(\\)"
-
-removeDisabled :: String -> String
-removeDisabled input = if after' == "" then before' else before' ++ removeDisabled after'
-  where
-    (before', _, after') = findDisabled input
-
-
 solve1 :: String -> Integer
-solve1 input = sum $ map (multiply . findMultipliers) $ findMul input
-
-solve2 :: String -> Integer
-solve2 input = sum $ map (multiply . findMultipliers) $ findMul $ removeDisabled input
+solve1 input = 1
 
 main :: IO ()
 main = do
   input <- readFile inputPath
-  print $ solve2 input
+  print $ solve1 input
