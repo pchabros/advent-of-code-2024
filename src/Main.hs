9b62b9a5d6d616d17eee7b4ec603cfc3e4c3cc21
pchabros <pawel.chabros@yahoo.pl>
2024-12-09 09:06:23 +0100

    Day 8: first puzzle solved

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,58 @@
+{-# LANGUAGE TupleSections #-}
+
 module Main where
 
+import Data.List (elemIndices, nub)
+
+pairs :: [a] -> [(a, a)]
+pairs [] = []
+pairs (x : xs) = map (x,) xs ++ pairs xs
+
 inputPath :: String
-inputPath = "./inputs/day-5.txt"
+inputPath = "./inputs/day-8.txt"
+
+type Frequency = Char
+
+type Map = [[Char]]
+
+type Location = (Int, Int)
+
+parseMap :: String -> [[Char]]
+parseMap = lines
+
+getFrequencies :: Map -> [Frequency]
+getFrequencies = nub . concatMap (filter (/= '.'))
+
+getLocations :: Map -> Frequency -> [Location]
+getLocations frequencyMap frequency = concatMap getRowLocations enumeratedRow
+  where
+    getRowLocations (row, i) = zip [i] (elemIndices frequency row)
+    enumeratedRow = zip frequencyMap [0 :: Int ..]
+
+getAllAntinodes :: (Location, Location) -> [Location]
+getAllAntinodes ((y1, x1), (y2, x2)) = [(y1 - dy, x1 - dx), (y2 + dy, x2 + dx)]
+  where
+    dx = x2 - x1
+    dy = y2 - y1
+
+isWithinMap :: Map -> Location -> Bool
+isWithinMap frequencyMap (y, x) = yWithinMap && xWithinMap
+  where
+    yWithinMap = y >= 0 && y < length frequencyMap
+    xWithinMap = x >= 0 && x < length (head frequencyMap)
+
+getFrequencyAninodes :: Map -> Frequency -> [Location]
+getFrequencyAninodes frequencyMap frequency = filter (isWithinMap frequencyMap) antinodesLocations
+  where
+    antinodesLocations = concatMap getAllAntinodes $ pairs (getLocations frequencyMap frequency)
+
+solve1 :: String -> Int
+solve1 input = length $ nub $ concatMap (getFrequencyAninodes frequencyMap) frequencies
+  where
+    frequencies = getFrequencies frequencyMap
+    frequencyMap = parseMap input
 
 main :: IO ()
 main = do
   input <- readFile inputPath
-  print input
+  print $ solve1 input
