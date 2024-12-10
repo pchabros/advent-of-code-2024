d587287b46c12581d1bab138eb9df00f95d34a2a
pchabros <pawel.chabros@yahoo.pl>
2024-12-10 11:23:59 +0100

    Day 8: second puzzle solved

--- src/Main.hs
+++ src/Main.hs
@@ -29,25 +29,27 @@ getLocations frequencyMap frequency = concatMap getRowLocations enumeratedRow
     getRowLocations (row, i) = zip [i] (elemIndices frequency row)
     enumeratedRow = zip frequencyMap [0 :: Int ..]
 
-getAllAntinodes :: (Location, Location) -> [Location]
-getAllAntinodes ((y1, x1), (y2, x2)) = [(y1 - dy, x1 - dx), (y2 + dy, x2 + dx)]
+getAntinodes :: Map -> (Location, Location) -> [Location]
+getAntinodes frequencyMap ((y1, x1), (y2, x2)) = zip top (if dx < 0 then right else left) ++ zip bot (if dx < 0 then left else right)
   where
+    top = [minY, minY - abs dy .. 0]
+    left = [minX, minX - abs dx .. 0]
+    bot = [maxY, maxY + abs dy .. boundaryY]
+    right = [maxX, maxX + abs dx .. boundaryX]
+    minX = min x1 x2
+    maxX = max x1 x2
+    minY = min y1 y2
+    maxY = max y1 y2
     dx = x2 - x1
     dy = y2 - y1
-
-isWithinMap :: Map -> Location -> Bool
-isWithinMap frequencyMap (y, x) = yWithinMap && xWithinMap
-  where
-    yWithinMap = y >= 0 && y < length frequencyMap
-    xWithinMap = x >= 0 && x < length (head frequencyMap)
+    boundaryX = length (head frequencyMap) - 1
+    boundaryY = length frequencyMap - 1
 
 getFrequencyAninodes :: Map -> Frequency -> [Location]
-getFrequencyAninodes frequencyMap frequency = filter (isWithinMap frequencyMap) antinodesLocations
-  where
-    antinodesLocations = concatMap getAllAntinodes $ pairs (getLocations frequencyMap frequency)
+getFrequencyAninodes frequencyMap frequency = concatMap (getAntinodes frequencyMap) $ pairs (getLocations frequencyMap frequency)
 
-solve1 :: String -> Int
-solve1 input = length $ nub $ concatMap (getFrequencyAninodes frequencyMap) frequencies
+solve2 :: String -> Int
+solve2 input = length $ nub $ concatMap (getFrequencyAninodes frequencyMap) frequencies
   where
     frequencies = getFrequencies frequencyMap
     frequencyMap = parseMap input
@@ -55,4 +57,4 @@ solve1 input = length $ nub $ concatMap (getFrequencyAninodes frequencyMap) freq
 main :: IO ()
 main = do
   input <- readFile inputPath
-  print $ solve1 input
+  print $ solve2 input
